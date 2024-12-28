3352e7723acaec759ad3196fea903eafa13ab653
Teo kamalipour <teokamalipour@gmail.com>
2024-12-29 01:58:59 +0330

    fix: basic auth added

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
     MONGODB_URI: process.env.MONGODB_URI,
     SHORT_DOMAIN: process.env.SHORT_DOMAIN,
     ORIGINAL_DOMAIN: process.env.ORIGINAL_DOMAIN,
+    ADMIN_USER: process.env.ADMIN_USER,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
   },
 };
 
---
+++ src/app/not-found.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button, Stack, Typography } from "@mui/material";
+
+import LottiePlayer from "react-lottie";
+import Animation from "@/lotties/searching.json";
+import Link from "next/link";
+export default function NotFound() {
+  return (
+    <Stack
+      direction={"row"}
+      width={"100%"}
+      height={"80vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Stack gap={2} alignItems={"center"}>
+        <LottiePlayer
+          width={300}
+          height={300}
+          options={{
+            animationData: Animation,
+            loop: true,
+            autoplay: true,
+          }}
+        />
+        <Typography variant="h5">!صفحه مورد نظر پیدا نشد</Typography>
+        <Button
+          LinkComponent={Link}
+          variant="contained"
+          color="primary"
+          fullWidth
+          href={process.env.ORIGINAL_DOMAIN}
+        >
+          بازگشت به خانه
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
